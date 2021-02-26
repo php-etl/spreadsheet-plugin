2356a55f0b260c40b5cfb424fe0a4910806917b5
sebprt <sebastien.parrat@orange.fr>
2021-02-26 15:26:22 +0100

    Updated dependencies with php-etl/phpunit-extension and updated tests

--- tests/functional/Builder/ExtractorTest.php
+++ tests/functional/Builder/ExtractorTest.php
@@ -2,11 +2,30 @@
 
 namespace functional\Kiboko\Plugin\Spreadsheet\Builder;
 
+use Kiboko\Component\PHPUnitExtension\BuilderAssertTrait;
 use Kiboko\Plugin\Spreadsheet\Builder;
 use Kiboko\Plugin\Log;
+use PHPUnit\Framework\TestCase;
+use Vfs\FileSystem;
 
-final class ExtractorTest extends BuilderTestCase
+final class ExtractorTest extends TestCase
 {
+    use BuilderAssertTrait;
+
+    private ?FileSystem $fs = null;
+
+    protected function setUp(): void
+    {
+        $this->fs = FileSystem::factory('vfs://');
+        $this->fs->mount();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->fs->unmount();
+        $this->fs = null;
+    }
+
     public function testWithFilePath(): void
     {
         $extract = new Builder\XLSX\Extractor(
