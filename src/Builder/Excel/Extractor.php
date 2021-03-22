69412e794946142cde928209ab9828d7bae50933
sebprt <sebastien.parrat@orange.fr>
2021-03-22 17:33:31 +0100

    added missing files and updated logger option

--- src/Builder/Excel/Extractor.php
+++ src/Builder/Excel/Extractor.php
@@ -16,7 +16,7 @@ class Extractor implements Builder
     ) {
     }
 
-    public function withLogger(?Node\Expr $logger): self
+    public function withLogger(Node\Expr $logger): self
     {
         $this->logger = $logger;
 
@@ -25,61 +25,61 @@ class Extractor implements Builder
 
     public function getNode(): Node
     {
-        $instance = new Node\Expr\New_(
-            class: new Node\Name\FullyQualified('Kiboko\\Component\\Flow\\Spreadsheet\\Sheet\\Safe\\Extractor'),
-            args: [
-                new Node\Arg(
-                    new Node\Expr\FuncCall(
-                        new Node\Expr\Closure(
-                            subNodes: [
-                                'stmts' => [
-                                    new Node\Stmt\Expression(
-                                        new Node\Expr\Assign(
-                                            new Node\Expr\Variable('reader'),
-                                            new Node\Expr\StaticCall(
-                                                class: new Node\Name\FullyQualified('Box\Spout\Reader\Common\Creator\ReaderEntityFactory'),
-                                                name: 'createXLSXReader'
-                                            ),
+        $arguments = [
+            new Node\Arg(
+                new Node\Expr\FuncCall(
+                    new Node\Expr\Closure(
+                        subNodes: [
+                        'stmts' => [
+                            new Node\Stmt\Expression(
+                                new Node\Expr\Assign(
+                                    new Node\Expr\Variable('reader'),
+                                    new Node\Expr\StaticCall(
+                                        class: new Node\Name\FullyQualified('Box\Spout\Reader\Common\Creator\ReaderEntityFactory'),
+                                        name: 'createXLSXReader'
+                                    ),
+                                ),
+                            ),
+                            new Node\Stmt\Expression(
+                                new Node\Expr\MethodCall(
+                                    var: new Node\Expr\Variable('reader'),
+                                    name: new Node\Identifier('open'),
+                                    args: [
+                                        new Node\Arg(
+                                            new Node\Scalar\String_($this->filePath)
                                         ),
-                                    ),
-                                    new Node\Stmt\Expression(
-                                        new Node\Expr\MethodCall(
-                                            var: new Node\Expr\Variable('reader'),
-                                            name: new Node\Identifier('open'),
-                                            args: [
-                                                new Node\Arg(
-                                                    new Node\Scalar\String_($this->filePath)
-                                                ),
-                                            ]
-                                        )
-                                    ),
-                                    new Node\Stmt\Return_(
-                                        new Node\Expr\Variable('reader')
-                                    ),
-                                ],
-                            ],
-                        ),
+                                    ]
+                                )
+                            ),
+                            new Node\Stmt\Return_(
+                                new Node\Expr\Variable('reader')
+                            ),
+                        ],
+                    ],
                     ),
                 ),
-                new Node\Arg(
-                    new Node\Scalar\String_($this->sheet)
-                ),
-                new Node\Arg(
-                    new Node\Scalar\LNumber($this->skipLine)
-                )
-            ]
-        );
+            ),
+            new Node\Arg(
+                new Node\Scalar\String_($this->sheet)
+            ),
+            new Node\Arg(
+                new Node\Scalar\LNumber($this->skipLine)
+            ),
+        ];
 
         if ($this->logger !== null) {
-            return new Node\Expr\MethodCall(
-                var: $instance,
-                name: 'setLogger',
-                args: [
-                    new Node\Arg($this->logger),
-                ]
+            array_push(
+                $arguments,
+                new Node\Arg(
+                    value: $this->logger,
+                    name: new Node\Identifier('logger'),
+                ),
             );
         }
 
-        return $instance;
+        return new Node\Expr\New_(
+            class: new Node\Name\FullyQualified('Kiboko\\Component\\Flow\\Spreadsheet\\Sheet\\Safe\\Extractor'),
+            args: $arguments
+        );
     }
 }
