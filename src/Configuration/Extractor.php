9b6b34e9a4be3efb1b64b3b5dd30a3cd0e6f4d24
clemzarch <clessili@gmail.com>
2021-12-03 12:10:52 +0100

    use mutuallyExclusiveFields and mutuallyDependentFields functions from package 'satellite-toolbox'

--- src/Configuration/Extractor.php
+++ src/Configuration/Extractor.php
@@ -6,6 +6,7 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use function Kiboko\Component\SatelliteToolbox\Configuration\asExpression;
 use function Kiboko\Component\SatelliteToolbox\Configuration\isExpression;
+use function Kiboko\Component\SatelliteToolbox\Configuration\mutuallyExclusiveFields;
 
 final class Extractor implements ConfigurationInterface
 {
@@ -15,7 +16,7 @@ final class Extractor implements ConfigurationInterface
 
         $builder->getRootNode()
             ->validate()
-                ->always($this->mutuallyExclusiveFields('excel', 'open_document', 'csv'))
+                ->always(mutuallyExclusiveFields('excel', 'open_document', 'csv'))
             ->end()
             ->children()
                 ->scalarNode('file_path')
@@ -108,26 +109,5 @@ final class Extractor implements ConfigurationInterface
             ->end();
 
         return $builder;
-    }
-
-    private function mutuallyExclusiveFields(string $field, string ...$exclusions): \Closure
-    {
-        return function (array $value) use ($field, $exclusions) {
-            if (!array_key_exists($field, $value)) {
-                return $value;
-            }
-
-            foreach ($exclusions as $exclusion) {
-                if (array_key_exists($exclusion, $value)) {
-                    throw new \InvalidArgumentException(sprintf(
-                        'Your configuration should either contain the "%s" or the "%s" field, not both.',
-                        $field,
-                        $exclusion,
-                    ));
-                }
-            }
-
-            return $value;
-        };
     }
 }
